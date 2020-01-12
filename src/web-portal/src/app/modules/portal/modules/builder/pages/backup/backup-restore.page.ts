6a69a20f79d87e9698ce4070ad0fa43b0af555a4
Johnathan Le <phucan1108@gmail.com>
2020-01-12 15:46:50 +0700

    Add some backup builders page

---
+++ src/web-portal/src/app/modules/portal/modules/builder/pages/backup/backup-restore.page.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'let-backup-restore',
+    templateUrl: './backup-restore.page.html'
+})
+export class BackupRestorePage implements OnInit {
+    constructor() { }
+
+    ngOnInit(): void { }
+}
---
+++ src/web-portal/src/app/modules/portal/modules/builder/pages/backup/backup-upload.page.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'backup-upload',
+    templateUrl: './backup-upload.page.html'
+})
+export class BackupUploadpage implements OnInit {
+    constructor() { }
+
+    ngOnInit(): void { }
+}
