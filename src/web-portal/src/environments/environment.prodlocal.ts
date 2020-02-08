04757dab0e96b69831ce3469f027ce2a483f138a
Johnathan Le <phucan1108@gmail.com>
2020-02-08 19:58:19 +0700

    Adjust local env

--- src/web-portal/src/environments/environment.prodlocal.ts
+++ src/web-portal/src/environments/environment.prodlocal.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     version: "0.0.5",
-    configurationEndpoint: "http://letportal.api/v1.0/api/configurations/Portal/v1.0",
+    configurationEndpoint: "http://letportal.app:8080/v1.0/api/configurations/Portal/v1.0",
     ignoreSendTokenEndpoints: "api/accounts/login;api/accounts/refresh;api/accounts/forgot-password;api/accounts/recovery-password"
   };
   
