8b986db689238851a7c7c3b701fe31a5aee906d9
Johnathan Le <phucan1108@gmail.com>
2020-02-10 00:37:38 +0700

    Add more console log

--- src/web-apis/LetPortal.ServiceManagementApis/Program.cs
+++ src/web-apis/LetPortal.ServiceManagementApis/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -9,6 +10,7 @@ namespace LetPortal.ServiceManagementApis
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Service Management is starting...");
             CreateHostBuilder(args).Build().Run();
         }
 
@@ -17,8 +19,10 @@ namespace LetPortal.ServiceManagementApis
             return Host.CreateDefaultBuilder(args)
                 .ConfigureAppConfiguration((hostingContext, config) =>
                 {
+                    Console.WriteLine("Collecting all files...");
                     config.SetBasePath(hostingContext.HostingEnvironment.ContentRootPath);
                     var path = Path.Combine(hostingContext.HostingEnvironment.ContentRootPath, "Files");
+                    Console.WriteLine("Path: " + path);
                     config.AddServicePerDirectory(path, hostingContext.HostingEnvironment.EnvironmentName);
                 })
                 .ConfigureWebHostDefaults(webBuilder =>
