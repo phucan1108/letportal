8b9cee209cb1c5f6992328011d6fa7dba1d7c119
Johnathan Le <phucan1108@gmail.com>
2020-03-06 23:13:15 +0700

    Change versions

--- src/web-portal/src/environments/environment.prod.ts
+++ src/web-portal/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  version: "0.0.5",
+  version: "0.5.0",
   configurationEndpoint: "http://192.168.20.6:8080/v1.0/api/configurations/Portal/v1.0",
   ignoreSendTokenEndpoints: "api/accounts/login;api/accounts/refresh;api/accounts/forgot-password;api/accounts/recovery-password"
 };
