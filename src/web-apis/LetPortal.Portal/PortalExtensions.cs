156eedfc38aeaf95a76131586460cee5bcc93aa1
Johnathan Le <phucan1108@gmail.com>
2020-05-21 16:14:28 +0700

    Add two CLIs: pack and unpack

--- src/web-apis/LetPortal.Portal/PortalExtensions.cs
+++ src/web-apis/LetPortal.Portal/PortalExtensions.cs
@@ -64,54 +64,9 @@ namespace LetPortal.Portal
             builder.Services.Configure<MapperOptions>(builder.Configuration.GetSection("MapperOptions"));
             builder.Services.Configure<BackupOptions>(builder.Configuration.GetSection("BackupOptions"));
             var mapperOptions = builder.Configuration.GetSection("MapperOptions").Get<MapperOptions>();
+            var databaseOptions = builder.Configuration.GetSection("DatabaseOptions").Get<DatabaseOptions>();
             builder.Services.AddSingleton(mapperOptions);
-            if (builder.ConnectionType == ConnectionType.MongoDB)
-            {
-                MongoDbRegistry.RegisterEntities();
-
-                // Register all mongo repositories
-                builder.Services.AddSingleton<IDatabaseRepository, DatabaseMongoRepository>();
-                builder.Services.AddSingleton<IDatasourceRepository, DatasourceMongoRepository>();
-                builder.Services.AddSingleton<IEntitySchemaRepository, EntitySchemaMongoRepository>();
-                builder.Services.AddSingleton<IAppRepository, AppMongoRepository>();
-                builder.Services.AddSingleton<IAppVersionRepository, AppVersionMongoRepository>();
-                builder.Services.AddSingleton<IPageRepository, PageMongoRepository>();
-                builder.Services.AddSingleton<IDynamicListRepository, DynamicListMongoRepository>();
-                builder.Services.AddSingleton<IStandardRepository, StandardMongoRepository>();
-                builder.Services.AddSingleton<IFileRepository, FileMongoRepository>();
-                builder.Services.AddSingleton<IChartRepository, ChartMongoRepository>();
-                builder.Services.AddSingleton<IBackupRepository, BackupMongoRepository>();
-                builder.Services.AddSingleton<IVersionRepository, VersionMongoRepository>();
-                builder.Services.AddSingleton<ILocalizationRepository, LocalizationMongoRepository>();
-
-                builder.Services.AddSingleton<IExecutionChartReport, MongoExecutionChartReport>();
-                builder.Services.AddSingleton<IMongoQueryExecution, MongoQueryExecution>();
-            }
-
-            if (builder.ConnectionType == ConnectionType.SQLServer
-                || builder.ConnectionType == ConnectionType.PostgreSQL
-                || builder.ConnectionType == ConnectionType.MySQL)
-            {
-                builder.Services.AddTransient<PortalDbContext>();
-                builder.Services.AddTransient<DbContext>((serviceProvider) =>
-                {
-                    return serviceProvider.GetService<PortalDbContext>();
-                });
-                // Register all EF repositories
-                builder.Services.AddTransient<IDatabaseRepository, DatabaseEFRepository>();
-                builder.Services.AddTransient<IDatasourceRepository, DatasourceEFRepository>();
-                builder.Services.AddTransient<IEntitySchemaRepository, EntitySchemaEFRepository>();
-                builder.Services.AddTransient<IAppRepository, AppEFRepository>();
-                builder.Services.AddTransient<IPageRepository, PageEFRepository>();
-                builder.Services.AddTransient<IDynamicListRepository, DynamicListEFRepository>();
-                builder.Services.AddTransient<IStandardRepository, StandardEFRepository>();
-                builder.Services.AddTransient<IFileRepository, FileEFRepository>();
-                builder.Services.AddTransient<IChartRepository, ChartEFRepository>();
-                builder.Services.AddTransient<IBackupRepository, BackupEFRepository>();
-                builder.Services.AddTransient<IVersionRepository, VersionEFRepository>();
-                builder.Services.AddTransient<ILocalizationRepository, LocalizationEFRepository>();
-            }
-
+            RegisterRepos(builder.Services, databaseOptions);
             if (portalOptions.EnableFileServer)
             {
                 builder.Services.Configure<FilePublishOptions>(builder.Configuration.GetSection("FilePublishOptions"));
@@ -131,78 +86,140 @@ namespace LetPortal.Portal
                 builder.Services.AddTransient<IStoreFileDatabase, PostgreStoreFileDatabase>();
                 builder.Services.AddTransient<IStoreFileDatabase, MongoStoreFileDatabase>();
             }
-
-            if (builder.ConnectionType == ConnectionType.MongoDB)
-            {
-                builder.Services.AddTransient<IExecutionDatabase, MongoExecutionDatabase>();
-                builder.Services.AddTransient<IExtractionDatabase, MongoExtractionDatabase>();
-                builder.Services.AddTransient<IExtractionDatasource, MongoExtractionDatasource>();
-                builder.Services.AddTransient<IDynamicListQueryDatabase, MongoDynamicListQueryDatabase>();
-                builder.Services.AddTransient<IAnalyzeDatabase, MongoAnalyzeDatabase>();
-            }
-            else if (builder.ConnectionType == ConnectionType.PostgreSQL)
-            {
-                builder.Services.AddTransient<IAnalyzeDatabase, PostgreAnalyzeDatabase>();
-                builder.Services.AddTransient<IExecutionDatabase, PostgreExecutionDatabase>();
-                builder.Services.AddTransient<IExtractionDatabase, PostgreExtractionDatabase>();
-                builder.Services.AddTransient<IDynamicListQueryDatabase, PostgreDynamicListQueryDatabase>();
-                builder.Services.AddTransient<IExtractionDatasource, PostgreExtractionDatasource>();
-                builder.Services.AddTransient<IExecutionChartReport, PostgreExecutionChartReport>();
-                builder.Services.AddTransient<IExtractionChartQuery, PostgreExtractionChartQuery>();
-
-                builder.Services.AddSingleton<IPostgreSqlMapper, PostgreSqlMapper>();
-            }
-            else if (builder.ConnectionType == ConnectionType.SQLServer)
-            {
-                builder.Services.AddTransient<IAnalyzeDatabase, SqlServerAnalyzeDatabase>();
-                builder.Services.AddTransient<IExecutionDatabase, SqlServerExecutionDatabase>();
-                builder.Services.AddTransient<IExtractionDatabase, SqlServerExtractionDatabase>();
-                builder.Services.AddTransient<IDynamicListQueryDatabase, SqlServerDynamicListQueryDatabase>();
-                builder.Services.AddTransient<IExtractionDatasource, SqlServerExtractionDatasource>();
-                builder.Services.AddTransient<IExecutionChartReport, SqlServerExecutionChartReport>();
-                builder.Services.AddTransient<IExtractionChartQuery, SqlServerExtractionChartQuery>();
-
-                builder.Services.AddSingleton<ISqlServerMapper, SqlServerMapper>();
-            }
-            else if (builder.ConnectionType == ConnectionType.MySQL)
-            {
-                builder.Services.AddTransient<IAnalyzeDatabase, MySqlAnalyzeDatabase>();
-                builder.Services.AddTransient<IExecutionDatabase, MySqlExecutionDatabase>();
-                builder.Services.AddTransient<IExtractionDatabase, MySqlExtractionDatabase>();
-                builder.Services.AddTransient<IDynamicListQueryDatabase, MySqlDynamicListQueryDatabase>();
-                builder.Services.AddTransient<IExtractionDatasource, MySqlExtractionDatasource>();
-                builder.Services.AddTransient<IExecutionChartReport, MySqlExecutionChartReport>();
-                builder.Services.AddTransient<IExtractionChartQuery, MySqlExtractionChartQuery>();
-
-                builder.Services.AddSingleton<IMySqlMapper, MySqlMapper>();
-            }
             builder.Services.AddSingleton<ICSharpMapper, CSharpMapper>();
-
-            builder.Services.AddTransient<IDatabaseServiceProvider, InternalDatabaseServiceProvider>();
-            builder.Services.AddTransient<IEntitySchemaServiceProvider, InternalEntitySchemaServiceProvider>();
-            builder.Services.AddTransient<IPageServiceProvider, InternalPageServiceProvider>();
-            builder.Services.AddTransient<IAppServiceProvider, InternalAppServiceProvider>();
-            builder.Services.AddTransient<IStandardServiceProvider, InternalStandardServiceProvider>();
-            builder.Services.AddTransient<IChartServiceProvider, InternalChartServiceProvider>();
-            builder.Services.AddTransient<IDynamicListServiceProvider, InternalDynamicListServiceProvider>();
-            builder.Services.AddTransient<IFileSeviceProvider, InternalFileServiceProvider>();
-            builder.Services.AddTransient<ILocalizationProvider, InternalLocalizationProvider>();
 
             builder.Services.AddTransient<IDynamicQueryBuilder, DynamicQueryBuilder>();
             builder.Services.AddTransient<IChartReportProjection, ChartReportProjection>();
             builder.Services.AddTransient<IChartReportQueryBuilder, ChartReportQueryBuilder>();
-
-            builder.Services.AddTransient<IEntitySchemaService, EntitySchemaService>();
-            builder.Services.AddTransient<IDynamicListService, DynamicListService>();
-            builder.Services.AddTransient<IDatasourceService, DatasourceService>();
-            builder.Services.AddTransient<IDatabaseService, DatabaseService>();
-            builder.Services.AddTransient<IFileService, FileService>();
-            builder.Services.AddTransient<IChartService, ChartService>();
-            builder.Services.AddTransient<IBackupService, BackupService>();
-            builder.Services.AddHttpClient<IHttpService, HttpService>();
-            builder.Services.AddTransient<IAppService, AppService>();
+            RegisterProviders(builder.Services);
+            RegisterServices(builder.Services);
 
             return builder;
+        }
+
+        public static void RegisterRepos(IServiceCollection services, DatabaseOptions databaseOptions, bool skipMongoRegister = false)
+        {
+            if (databaseOptions.ConnectionType == ConnectionType.MongoDB)
+            {
+                if (!skipMongoRegister)
+                {
+                    MongoDbRegistry.RegisterEntities();
+                }                                                      
+
+                // Register all mongo repositories
+                services.AddSingleton<IDatabaseRepository, DatabaseMongoRepository>();
+                services.AddSingleton<IDatasourceRepository, DatasourceMongoRepository>();
+                services.AddSingleton<IEntitySchemaRepository, EntitySchemaMongoRepository>();
+                services.AddSingleton<IAppRepository, AppMongoRepository>();
+                services.AddSingleton<IAppVersionRepository, AppVersionMongoRepository>();
+                services.AddSingleton<IPageRepository, PageMongoRepository>();
+                services.AddSingleton<IDynamicListRepository, DynamicListMongoRepository>();
+                services.AddSingleton<IStandardRepository, StandardMongoRepository>();
+                services.AddSingleton<IFileRepository, FileMongoRepository>();
+                services.AddSingleton<IChartRepository, ChartMongoRepository>();
+                services.AddSingleton<IBackupRepository, BackupMongoRepository>();
+                services.AddSingleton<IVersionRepository, VersionMongoRepository>();
+                services.AddSingleton<ILocalizationRepository, LocalizationMongoRepository>();
+
+                services.AddSingleton<IExecutionChartReport, MongoExecutionChartReport>();
+                services.AddSingleton<IMongoQueryExecution, MongoQueryExecution>();
+            }
+
+            if (databaseOptions.ConnectionType == ConnectionType.SQLServer
+                || databaseOptions.ConnectionType == ConnectionType.PostgreSQL
+                || databaseOptions.ConnectionType == ConnectionType.MySQL)
+            {
+                services.AddTransient<PortalDbContext>();
+                services.AddTransient<DbContext>((serviceProvider) =>
+                {
+                    return serviceProvider.GetService<PortalDbContext>();
+                });
+                // Register all EF repositories
+                services.AddTransient<IDatabaseRepository, DatabaseEFRepository>();
+                services.AddTransient<IDatasourceRepository, DatasourceEFRepository>();
+                services.AddTransient<IEntitySchemaRepository, EntitySchemaEFRepository>();
+                services.AddTransient<IAppRepository, AppEFRepository>();
+                services.AddTransient<IPageRepository, PageEFRepository>();
+                services.AddTransient<IDynamicListRepository, DynamicListEFRepository>();
+                services.AddTransient<IStandardRepository, StandardEFRepository>();
+                services.AddTransient<IFileRepository, FileEFRepository>();
+                services.AddTransient<IChartRepository, ChartEFRepository>();
+                services.AddTransient<IBackupRepository, BackupEFRepository>();
+                services.AddTransient<IVersionRepository, VersionEFRepository>();
+                services.AddTransient<ILocalizationRepository, LocalizationEFRepository>();
+            }
+
+            if (databaseOptions.ConnectionType == ConnectionType.MongoDB)
+            {
+                services.AddTransient<IExecutionDatabase, MongoExecutionDatabase>();
+                services.AddTransient<IExtractionDatabase, MongoExtractionDatabase>();
+                services.AddTransient<IExtractionDatasource, MongoExtractionDatasource>();
+                services.AddTransient<IDynamicListQueryDatabase, MongoDynamicListQueryDatabase>();
+                services.AddTransient<IAnalyzeDatabase, MongoAnalyzeDatabase>();
+            }
+            else if (databaseOptions.ConnectionType == ConnectionType.PostgreSQL)
+            {
+                services.AddTransient<IAnalyzeDatabase, PostgreAnalyzeDatabase>();
+                services.AddTransient<IExecutionDatabase, PostgreExecutionDatabase>();
+                services.AddTransient<IExtractionDatabase, PostgreExtractionDatabase>();
+                services.AddTransient<IDynamicListQueryDatabase, PostgreDynamicListQueryDatabase>();
+                services.AddTransient<IExtractionDatasource, PostgreExtractionDatasource>();
+                services.AddTransient<IExecutionChartReport, PostgreExecutionChartReport>();
+                services.AddTransient<IExtractionChartQuery, PostgreExtractionChartQuery>();
+
+                services.AddSingleton<IPostgreSqlMapper, PostgreSqlMapper>();
+            }
+            else if (databaseOptions.ConnectionType == ConnectionType.SQLServer)
+            {
+                services.AddTransient<IAnalyzeDatabase, SqlServerAnalyzeDatabase>();
+                services.AddTransient<IExecutionDatabase, SqlServerExecutionDatabase>();
+                services.AddTransient<IExtractionDatabase, SqlServerExtractionDatabase>();
+                services.AddTransient<IDynamicListQueryDatabase, SqlServerDynamicListQueryDatabase>();
+                services.AddTransient<IExtractionDatasource, SqlServerExtractionDatasource>();
+                services.AddTransient<IExecutionChartReport, SqlServerExecutionChartReport>();
+                services.AddTransient<IExtractionChartQuery, SqlServerExtractionChartQuery>();
+
+                services.AddSingleton<ISqlServerMapper, SqlServerMapper>();
+            }
+            else if (databaseOptions.ConnectionType == ConnectionType.MySQL)
+            {
+                services.AddTransient<IAnalyzeDatabase, MySqlAnalyzeDatabase>();
+                services.AddTransient<IExecutionDatabase, MySqlExecutionDatabase>();
+                services.AddTransient<IExtractionDatabase, MySqlExtractionDatabase>();
+                services.AddTransient<IDynamicListQueryDatabase, MySqlDynamicListQueryDatabase>();
+                services.AddTransient<IExtractionDatasource, MySqlExtractionDatasource>();
+                services.AddTransient<IExecutionChartReport, MySqlExecutionChartReport>();
+                services.AddTransient<IExtractionChartQuery, MySqlExtractionChartQuery>();
+
+                services.AddSingleton<IMySqlMapper, MySqlMapper>();
+            }
+        }
+
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddTransient<IEntitySchemaService, EntitySchemaService>();
+            services.AddTransient<IDynamicListService, DynamicListService>();
+            services.AddTransient<IDatasourceService, DatasourceService>();
+            services.AddTransient<IDatabaseService, DatabaseService>();
+            services.AddTransient<IFileService, FileService>();
+            services.AddTransient<IChartService, ChartService>();
+            services.AddTransient<IBackupService, BackupService>();
+            services.AddHttpClient<IHttpService, HttpService>();
+            services.AddTransient<IAppService, AppService>();
+
+        }
+
+        public static void RegisterProviders(IServiceCollection services)
+        {   
+            services.AddTransient<IDatabaseServiceProvider, InternalDatabaseServiceProvider>();
+            services.AddTransient<IEntitySchemaServiceProvider, InternalEntitySchemaServiceProvider>();
+            services.AddTransient<IPageServiceProvider, InternalPageServiceProvider>();
+            services.AddTransient<IAppServiceProvider, InternalAppServiceProvider>();
+            services.AddTransient<IStandardServiceProvider, InternalStandardServiceProvider>();
+            services.AddTransient<IChartServiceProvider, InternalChartServiceProvider>();
+            services.AddTransient<IDynamicListServiceProvider, InternalDynamicListServiceProvider>();
+            services.AddTransient<IFileSeviceProvider, InternalFileServiceProvider>();
+            services.AddTransient<ILocalizationProvider, InternalLocalizationProvider>();
         }
     }
 }
--- src/web-apis/LetPortal.Portal/Services/Apps/IAppService.cs
+++ src/web-apis/LetPortal.Portal/Services/Apps/IAppService.cs
@@ -13,6 +13,13 @@ namespace LetPortal.Portal.Services.Apps
         Task Install(string uploadFileId, InstallWay installWay = InstallWay.Merge);
 
         Task<PreviewPackageModel> Preview(string appId);
+
+        #region Supports CLI
+        Task<string> Package(string appName, string saveFolderPath);
+
+        Task Unpack(string zipFilePath, InstallWay installWay = InstallWay.Merge);
+
+        #endregion
     }
 
     public enum InstallWay
